3af1e10ed8aa13f090089469cf9b9abaffdee469
KotoJallow <kotojalloh.kj@gmail.com>
2025-08-25 14:47:21 +0000

    discard globalcss import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';// Import the global.css file in the index.js file:
-import './global.css';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation';
 import { Provider } from 'react-redux';
